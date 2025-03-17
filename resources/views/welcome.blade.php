4a4cc1dbbbee68e0d33c8c6438c3bdde510e0f7a
lustycru <jneveu@edenschool.fr>
2025-03-17 14:24:02 +0100

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -7,10 +6,6 @@
     <meta name="description" content="">
     <title>Frontend Quiz</title>
     <link rel="stylesheet" href="../css/app.css">
-    <link
-        href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css"
-        rel="stylesheet"
-    />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
