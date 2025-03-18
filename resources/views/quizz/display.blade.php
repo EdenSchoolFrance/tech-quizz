c7a7371073f71f015e580a46cac44ed5c1da3890
Lenybare <cfontaine@edenschool.fr>
2025-03-18 16:39:46 +0100

    Finishing view display

--- resources/views/quizz/display.blade.php
+++ resources/views/quizz/display.blade.php
@@ -1,3 +1,16 @@
-<div>
-   
-</div>
+@extends('layouts.quizz')
+<main class="size-full flex justify-center items-center">
+    <div class="w-[50%] flex justify-center items-center">
+        <div class="w-[50%]">
+            <h1>Welcome to the <span>Frontend Quiz!</span></h1>
+            <p>Pick a subject to get started.</p>
+        </div>
+        <div class="w-[50%]">
+            @foreach ($quizz as $subject)
+                <a href="{{ route('quiz.start', $subject->id) }}" class="button mt-10 bg-white">
+                    <img class="w-[15px] h-[15px]" src="/public/img/quizz/{{ $subject->img }}">{{ $subject->name }}
+                </a>
+            @endforeach
+        </div>
+    </div>
+</main>
