a3e3b181ae93f6006f0a826faf007c8afbdaf259
TryaNever <alexispernette@outlook.fr>
2025-03-18 15:25:26 +0100

    preparing rebase

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -1,5 +1,13 @@
 <x-guest-layout>
-
+    <div>
+        <div>
+            <p>Welcome to the</p>
+            <h1>Frontend Quiz!</h1>
+        </div>
+        <p>Pick a subject to get started</p>
+    </div>
+    <div>
+    </div>
 
 
 </x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\QuizController;
 
 Route::get('/', [QuizController::class, 'index']);
 
