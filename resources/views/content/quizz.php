03e61b0868decd035b12169213e2b30d0166b29a
memirkocak <mekocak@edenschool>
2025-03-18 17:09:26 +0100

    SHOW QUIZZ FROM DB

--- resources/views/content/quizz.php
+++ resources/views/content/quizz.php
@@ -1,12 +1,12 @@
 <?php ob_start(); ?>
 
-    <section>
-        <h1>Welcome to the Frontend Quiz !</h1>
+    <section class="container mx-auto p-6">
+        <h1 class="text-3xl font-bold mb-6">Welcome to the Frontend Quiz !</h1>
         <article>
         <?php foreach ($quizz as $quiz): ?>
-            <div>
-                <h2><?= $quiz->getTitle() ?></h2>
-                <p><?= $quiz->getDescription() ?></p>   
+            <div class="bg-white shadow-md rounded-lg p-6 mb-4">
+                <h2 class="text-2xl font-semibold mb-2"><?= $quiz->getTitle() ?></h2>
+                <p class="text-gray-700"><?= $quiz->getDescription() ?></p>   
         <?php endforeach ?>
         </article>
     </section>
