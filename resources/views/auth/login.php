30a7d412a745d99e8be4e6512ea40489d39c2ad2
memirkocak <mekocak@edenschool>
2025-03-18 14:08:01 +0100

    form login

--- resources/views/auth/login.php
+++ resources/views/auth/login.php
@@ -4,5 +4,19 @@
     <h1>This is the login page</h1>
 </section>
 
+<form action="/postLogin" method="post">
+    <div>
+        <label for="username">username : </label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div>
+        <label for="password">password : </label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div>
+        <input type="submit" value="Login">
+    </div>
+</form>
+
 <?php $content = ob_get_clean(); ?>
 <?php require VIEWS . 'layout.php'; ?>
