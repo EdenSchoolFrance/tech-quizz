a37c2650f88fa4630614c5e3ae8f4df5e19bf7df
memirkocak <mekocak@edenschool>
2025-03-18 14:02:18 +0100

    register form

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -4,5 +4,23 @@
     <h1>This is the register page</h1>
 </section>
 
+<form action="/postRegister" method="post">
+    <div>
+        <label for="username">username : </label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div>
+        <label for="email">email : </label>
+        <input type="email" name="email" id="email">
+    </div>
+    <div>
+        <label for="password">password : </label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div>
+        <input type="submit" value="Register">
+    </div>
+</form>
+
 <?php $content = ob_get_clean(); ?>
 <?php require VIEWS . 'layout.php'; ?>
