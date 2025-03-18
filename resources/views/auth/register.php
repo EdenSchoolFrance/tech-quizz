48624ec9d7f044af101c5c1190be5bb24318cf04
memirkocak <mekocak@edenschool>
2025-03-18 14:36:30 +0100

    push update register(view)

--- resources/views/auth/register.php
+++ resources/views/auth/register.php
@@ -28,23 +28,5 @@
         </form>
 </section>
 
-<form action="/postRegister" method="post">
-    <div>
-        <label for="username">username : </label>
-        <input type="text" name="username" id="username">
-    </div>
-    <div>
-        <label for="email">email : </label>
-        <input type="email" name="email" id="email">
-    </div>
-    <div>
-        <label for="password">password : </label>
-        <input type="password" name="password" id="password">
-    </div>
-    <div>
-        <input type="submit" value="Register">
-    </div>
-</form>
-
 <?php $content = ob_get_clean(); ?>
 <?php require VIEWS . 'layout.php'; ?>
