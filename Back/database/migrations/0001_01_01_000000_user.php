a983ea0928c3c0f10ccfe6cf361c498ff572b41d
IronLegends7519 <drafin@edenschool.fr>
2025-03-18 16:42:28 +0100

    rajout migration role

--- Back/database/migrations/0001_01_01_000000_user.php
+++ Back/database/migrations/0001_01_01_000000_user.php
@@ -16,6 +16,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('role_id')->on('role');
             $table->timestamps();
         });
     }
