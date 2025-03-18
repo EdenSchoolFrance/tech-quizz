51bb74cc087a34f08fad9da7ac7e7e9e22442b73
HorroGlitch <lverot@edenschool.fr>
2025-03-18 16:32:00 +0100

    commit

--- public/index.php
+++ public/index.php
@@ -12,7 +12,6 @@ $router->get('/', 'HomeController@index');
 
 $router->get('/login', 'AuthController@showLogin');
 $router->get('/register', 'AuthController@showRegister');
-$router->get('/quizz', 'QuizController@index');
 $router->post('/postRegister', 'AuthController@postRegister');
 $router->post('/postLogin', 'AuthController@postLogin');
 
