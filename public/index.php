924a985d759aaf055c4f91040c13ad6f56adbedd
Auguste <adollinger@edenschool.fr>
2025-03-19 14:18:26 +0100

    added logout

--- public/index.php
+++ public/index.php
@@ -29,8 +29,10 @@ if(!auth() || user('role') == 'admin') {
 }
 
 
-if(auth()) {
+if(auth())
+{
 
+    $router->get('/logout', 'AuthController@logout');
     $router->get('/quiz', 'QuizController@index');
     $router->get('/result', 'ResultController@index');
     $router->get('/quiz/:id', 'QuestionController@show');
