fe0cda98eb8629e0b740273b5dbadb80ee2875cf
Auguste <adollinger@edenschool.fr>
2025-03-19 14:53:48 +0100

    Added remember me functionality

--- public/index.php
+++ public/index.php
@@ -1,49 +1,50 @@
 <?php
 require_once '../app/models/User.php';
 
-
-session_start();
-
 require '../config/config.php';
 require '../vendor/autoload.php';
 require APP . 'helper.php';
 
+use App\Router;
+use App\Models\UserManager;
+
+session_start();
+
 if (isset($_COOKIE['remember']) && !isset($_SESSION['user'])) {
-    $user = new App\Models\UserManager();
+    $user = new UserManager();
     $user = $user->getUser($_COOKIE['remember']);
     $_SESSION['user'] = $user;
 }
 
-$router = new App\Router($_SERVER["REQUEST_URI"]);
+$router = new Router($_SERVER["REQUEST_URI"]);
 
 $router->get('/', 'HomeController@index');
 
 if(user('role') == 'admin') {
     $router->get('/quiz/create', 'QuizController@create');
-    $router->get('/dashboard', 'AdminController@index');
+    $router->get('/dashboard', 'QuizController@dashboard');
     $router->post('/quiz/store', 'QuizController@store');
+    $router->post('/quiz/delete/:id', 'QuizController@delete');
 }
 
 
 if(!auth() || user('role') == 'admin') {
 
     $router->get('/login', 'AuthController@showLogin');
+    $router->post('/login', 'AuthController@login');
     $router->get('/register', 'AuthController@showRegister');
     $router->post('/register', 'AuthController@register');
-    $router->post('/login', 'AuthController@login');
 
 }
 
 
 if(auth())
 {
-
     $router->get('/logout', 'AuthController@logout');
     $router->get('/quiz', 'QuizController@index');
     $router->get('/result', 'ResultController@index');
     $router->get('/quiz/:id', 'QuestionController@show');
 }
-
 
 
 
