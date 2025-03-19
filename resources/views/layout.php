64e30cfb044a83ad1d57b2dfbb6c4fcc9690e440
Auguste <adollinger@edenschool.fr>
2025-03-19 15:34:19 +0100

    Made the quizz page front-end

--- resources/views/layout.php
+++ resources/views/layout.php
@@ -6,10 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>— YOUR_NAME —</title>
     <script src="https://kit.fontawesome.com/c1d0ab37d6.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="/style.css">
 </head>
-<body class="min-h-screen bg-white relative">
+<body class="min-h-screen bg-white relative flex flex-col">
     <div class="absolute top-0 left-0 -z-10">
         <img src="/assets/circle-top.png" alt="Background circle" class="w-full max-w-md opacity-10">
     </div>
@@ -43,7 +42,7 @@
 
         </ul>
     </header>
-    <main class="container mx-auto px-4 py-8">
+    <main class="container mx-auto px-4 py-8 grow flex flex-col justify-center">
         <?php echo $content; ?>
     </main>
 </body>
