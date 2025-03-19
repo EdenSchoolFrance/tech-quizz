4edc5c5afdc76f10081c04ee94141c8d82bf436a
memirkocak <mekocak@edenschool>
2025-03-19 14:08:20 +0100

    update quiz front

--- resources/views/content/quiz.php
+++ resources/views/content/quiz.php
@@ -1,12 +1,12 @@
 <?php ob_start(); ?>
 
 <section class="container mx-auto p-6">
-    <a href="quiz/create">Nouveau Quiz</a>
+    <a href="quiz/create" class="inline-block bg-blue-500 text-white px-4 py-2 rounded mb-6">Nouveau Quiz</a>
 
     <h1 class="text-3xl font-bold mb-6">Welcome to the Frontend Quiz!</h1>
     <article>
         <?php foreach ($quizz as $quiz): ?>
-            <a href="quiz/<?= $quiz->getId() ?>" class="bg-white shadow-md rounded-lg p-6 mb-4">
+            <a href="quiz/<?= $quiz->getId() ?>" class="block bg-white shadow-md rounded-lg p-6 mb-4 hover:bg-gray-100 transition">
                 <h2 class="text-2xl font-semibold mb-2"><?= $quiz->getTitle() ?></h2>
                 <p class="text-gray-700"><?= $quiz->getDescription() ?></p>
             </a>
