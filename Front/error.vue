6449ada9c91839fd038356b79b2d4b0f8778c50d
lroux07 <lroux@edenschool.fr>
2025-03-19 10:04:54 +0100

    Page d'erreur 404

--- Front/error.vue
+++ Front/error.vue
@@ -1,9 +1,14 @@
 <template>
-  <div>
-    <h1>Erreur</h1>
-    <p>{{ error?.statusCode }}</p>
-    <p>{{ error?.message }}</p>
-    <button @click="handleError">Retour à la page d'accueil</button>
+  <div class="bg-gray-200 text-gray-800 h-screen w-screen flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-between items-center bg-white p-14 rounded-lg shadow-md h-96">
+      <div>
+        <h1 class="font-bold text-8xl">{{ error?.statusCode }}</h1>
+      </div>
+      <div>
+        <p class="text-2xl font-normal mb-2">Looks like you’ve got lost….</p>
+        <button class="text-2xl hover:underline font-light bg-purple-600 text-white px-4 py-2 w-full mt-4 text-base rounded-lg" @click="handleError">Back to Home</button>
+      </div>
+    </div>
   </div>
 </template>
 
--- Front/pages/index.vue
+++ Front/pages/index.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="bg-gray-200 h-screen w-screen flex flex-col justify-center items-center">
+  <div class="bg-gray-200 text-gray-800 h-screen w-screen flex flex-col justify-center items-center">
     <div class="flex flex-col lg:flex-row flex-row gap-40">
       <div class="flex flex-col space-y-8">
-        <h1 class="text-5xl font-extralight text-gray-800">
+        <h1 class="text-5xl font-extralight">
           Welcom to the 
           <br> 
           <span class="font-bold">Frontend Quiz!</span>
         </h1>
-        <p class="text-gray-800 italic">Pick a subject to got started</p>
+        <p class="italic">Pick a subject to got started</p>
       </div>
 
       <ul class="flex flex-col gap-4 w-96">
         <li v-for="subject in quiz" :key="subject.id_quiz">
-          <NuxtLink class="text-black bg-white p-4 rounded-lg shadow-md flex flex-row items-center gap-2" :to="`${subject.title.replace(' ', '-')}/${subject.id_quiz}`">{{ subject.title }}</NuxtLink>
+          <NuxtLink class="bg-white p-4 rounded-lg shadow-md flex flex-row items-center gap-2" :to="`${subject.title.replace(' ', '-')}/${subject.id_quiz}`">{{ subject.title }}</NuxtLink>
         </li>
       </ul>
     </div>
