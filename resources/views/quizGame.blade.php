94477889041145a53966f03708b435d7a520f864
Aomigo <bingoash36@gmail.com>
2025-03-24 13:16:15 +0100

    style quiz

--- resources/views/quizGame.blade.php
+++ resources/views/quizGame.blade.php
@@ -16,41 +16,32 @@
         <img class="ellipse ellipse-1" src="{{ asset('img/ellipse1.svg') }}" alt="ellipse">
     </div>
     <div class="form">
-        <?php
-        function debug($param){
-            echo "<pre style='background-color: lightgray'>";
-            var_dump($param);
-            echo "</pre>";
-        }
-        debug($quiz);
-        debug($nameQuiz);
-        ?>
         <h1>{{ $nameQuiz }}</h1>
         <div>
             <p>Question {{ $quiz['nb_question'] }} of 10 </p>
             <h2>{{ $quiz['name_question'] }}</h2>
 
-            <input type="range" name="range" min="0" max="10" value="{{ $quiz['nb_question'] }}">
+            <input type="range" name="range" min="0" max="10" value="{{ $quiz['nb_question'] }}" disabled>
         </div>
         <div>
             <form action="/quiz/{{ $quiz['id_quiz'] }}/{{ $quiz['nb_question'] }}/result">
-                <div>
+                <div class="input">
                     <label for="f_answer">{{ $quiz['f_answer'] }}</label>
-                    <input type="radio" name="answer" id="f_answer" class="hidden">
+                    <input class="hidden" type="radio" name="answer" id="f_answer" class="hidden">
                 </div>
-                <div>
+                <div class="input">
                     <label for="s_answer">{{ $quiz['s_answer'] }}</label>
-                    <input type="radio" name="answer" id="s_answer" class="hidden">
+                    <input class="hidden" type="radio" name="answer" id="s_answer" class="hidden">
                 </div>
-                <div>
+                <div class="input">
                     <label for="t_answer">{{ $quiz['t_answer'] }}</label>
-                    <input type="radio" name="answer" id="t_answer" class="hidden">
+                    <input class="hidden" type="radio" name="answer" id="t_answer" class="hidden">
                 </div>
-                <div>
+                <div class="input">
                     <label for="fth-answer">{{ $quiz['fth-answer'] }}</label>
-                    <input type="radio" name="answer" id="fth-answer" class="hidden">
+                    <input class="hidden" type="radio" name="answer" id="fth-answer" class="hidden">
                 </div>
-                <input type="submit" value="Submit Answer">
+                <input class="button" type="submit" value="Submit Answer">
             </form>
         </div>
 
