ada2d4748de04f0897678e966937bde290f76818
Houdychou <hchouchouh@edenschool.fr>
2025-03-27 15:40:55 +0100

    middleware to check role finished

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\ResultsController;
@@ -16,6 +17,21 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::get('/quizz/{id}/question/{idQuestion}', [QuizController::class, 'showQuestions']);
     Route::get('/score/quizz/{id}', [QuizController::class, 'score']);
+
+    Route::get('/admin', function () {
+        var_dump("vous êtes un admin");
+    })->middleware([RoleMiddleware::class . ':admin']);
+
+    Route::get('/user', function () {
+        var_dump("vous êtes un user");
+    })->middleware([RoleMiddleware::class . ':user']);
+
+});
+
+Route::middleware([RoleMiddleware::class . ':admin'])->group(function () {
+    Route::get('/admin', function () {
+        var_dump("vous êtes un admin");
+    })->middleware([RoleMiddleware::class . ':admin']);
 });
 
 Route::get('/quizz/', [QuizController::class, 'index'])->name('quizzes.index');
