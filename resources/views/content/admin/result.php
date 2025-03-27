8e509f0dccd2d2c6931ede2490546eb05efcf216
HorroGlitch <lverot@edenschool.fr>
2025-03-27 14:19:42 +0100

    US-20 style UI done

--- resources/views/content/admin/result.php
+++ resources/views/content/admin/result.php
@@ -1,20 +1,19 @@
 <?php ob_start(); ?>
-    <h1 class="text-3xl font-bold mb-6 max-[376px]:hidden">Admin Dashboard</h1>
-    <section class="w-full mx-auto">
-        <h1 class="text-3xl font-bold mb-6 max-[376px]:hidden">Admin Dashboard</h1>
+    <h1 class="text-3xl font-bold mb-6 max-[376px]:hidden">User's result</h1>
+    <section class="w-full mx-auto rounded-3xl">
 
         <article>
             <?php if (empty($userResults)): ?>
             <p class="text-gray-500">This user haven't done any quiz yet.</p>
         <?php else: ?>
             <div class="overflow-x-auto ">
-                <table class="min-w-full divide-y divide-gray-200 ">
-                    <thead class="bg-gray-50 dark:bg-[#3B4D66] ">
+                <table class="min-w-full divide-y divide-gray-200  ">
+                    <thead class="bg-gray-50 dark:bg-[#3B4D66]  ">
                         <tr class="grid grid-cols-4">
-                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-neutral-50">Id</th>
-                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-neutral-50">Date</th>
-                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-neutral-50">Quiz Name</th>
-                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider dark:text-neutral-50">Score</th>
+                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider dark:text-neutral-50">ID</th>
+                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider dark:text-neutral-50">Date</th>
+                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider dark:text-neutral-50">Quiz Name</th>
+                            <th scope="col" class="xs:px-6 px-4 py-3 text-left text-xs font-medium text-gray-500 tracking-wider dark:text-neutral-50">Score</th>
                         </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200 dark:bg-[#3B4D66]">
