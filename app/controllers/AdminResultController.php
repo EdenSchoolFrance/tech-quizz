41fa64010b3f7553e07cbe178a809654ef926985
nffdev <nightfrost2discord@outlook.fr>
2025-03-28 15:41:46 +0100

    feat(app): added comments for AdminResultController

--- app/controllers/AdminResultController.php
+++ app/controllers/AdminResultController.php
@@ -4,20 +4,30 @@ namespace App\controllers;
 
 use App\models\AdminResultManager;
 
-
+/**
+ * Controller handling admin results related actions
+ */
 class AdminResultController
 {
     private $arc;
 
+    /**
+     * Initialize AdminResultManager
+     */
     public function __construct()
     {
         $this->arc = new AdminResultManager();
     }
 
+    /**
+     * Display user results
+     * 
+     * @param int $id User ID
+     * @return void
+     */
     public function show($id)
     {
         $userResults = $this->arc->get($id);
         require VIEWS . 'content/admin/result.php';
     }
-
 }
