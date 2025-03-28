b807b046aab241ede7ef30b99994680e567d7cb8
nffdev <nightfrost2discord@outlook.fr>
2025-03-28 15:43:58 +0100

    feat(app): added comments for AnswersController

--- app/controllers/AnswersController.php
+++ app/controllers/AnswersController.php
@@ -4,26 +4,41 @@ namespace App\controllers;
 
 use App\models\AnswersManager;
 
-
+/**
+ * Controller handling answers related actions
+ */
 class AnswersController
 {
     private $ac;
 
+    /**
+     * Initialize AnswersManager
+     */
     public function __construct()
     {
-        $this->ac = new QuestionManager();
+        $this->ac = new QuestionManager(); 
     }
 
+    /**
+     * Display all answers
+     * 
+     * @return void
+     */
     public function index()
     {
         $answers = $this->ac->getAll();
         require VIEWS . 'content/AffichageQuestion.php';
     }
 
+    /**
+     * Display a specific answer
+     * 
+     * @param int $id Answer ID
+     * @return void
+     */
     public function show($id)
     {
         $answer = $this->ac->get($id);
         require VIEWS . 'content/AffichageQuestion.php';
     }
-
 }
